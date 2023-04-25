218fb12f060d5604fa321d37bad22417ff19ce67
lwwilliam <lee1052william@gmail.com>
2023-04-25 21:32:17 +0800

    check for side walls

--- map_check.c
+++ map_check.c
@@ -6,27 +6,45 @@
 /*   By: lwilliam <lwilliam@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 18:59:39 by lwilliam          #+#    #+#             */
-/*   Updated: 2023/04/25 20:47:48 by lwilliam         ###   ########.fr       */
+/*   Updated: 2023/04/25 21:31:31 by lwilliam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	wall(char *line)
+int	wall_side(t_map *map)
+{
+	int	y;
+	int	x;
+
+	y = 0;
+	x = 0;
+	while (map->map[y][x])
+	{
+		
+	}
+}
+
+int	wall(t_map *map, char *line, int current_width)
 {
 	int	x;
 
 	x = 0;
-	while (line[x])
+	if (current_width == 0 || current_width == map->map_height)
 	{
-		if (line[x] != '1' && line[x] != ' ')
+		while (line[x])
 		{
-			ft_putstr_fd("\033[0;31mError!\nMap \
+			if (line[x] != '1' && line[x] != ' ')
+			{
+				ft_putstr_fd("\033[0;31mError!\nMap \
 is not closed by walls\033[0m\n", 2);
-			return (1);
+				return (1);
+			}
+			x++;
 		}
-		x++;
 	}
+	if (wall_side(map) == 1)
+		return (1);
 	return (0);
 }
 
@@ -52,8 +70,7 @@ int	map_check(t_map *map)
 	while (map->map[y])
 	{
 		x = 0;
-		// if (y == 0 || y == map->map_height)
-		if (wall(map->map[y]) == 1)
+		if (wall(map, map->map[y], y) == 1)
 			return (1);
 		while (map->map[y][x])
 		{
